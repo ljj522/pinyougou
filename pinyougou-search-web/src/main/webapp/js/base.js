e2fb69a0873cfe1d59484ea037a9e785a767630a
ljj522 <1030600039@qq.com>
2019-08-28 21:13:39 +0800

    第10天

---
+++ pinyougou-search-web/src/main/webapp/js/base.js
@@ -0,0 +1,8 @@
+var app=angular.module('pinyougou',[]);//自定义模块
+
+//定义过滤器
+app.filter('trustHtml',['$sce',function ($sce) {
+    return function (data) {//传入参数时被过滤的内容
+        return $sce.trustAsHtml(data);
+    }
+}]);
